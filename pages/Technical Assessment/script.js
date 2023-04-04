c43da80ee0892005c4e5f2e3ae689254b80d157c
Joel Siroy <joel.siroy@gmail.com>
2023-04-04 10:48:26 +0800

    techinical

--- pages/Technical Assessment/script.js
+++ pages/Technical Assessment/script.js
@@ -20,59 +20,57 @@
 // const state = "Nebraska";
 // const street = " Washington";
 // 
-// function userInformation = (a = (`First Name: $(firstName)`)), (b = (`Last Name: $(lastName)`)),
-//  (c= (`Age: $(age)`)),(d=(`Hobbies: (hobby $(hobbies)`)), (e = (`Relationships $(isMarried)`));
-//   {
-    //  return (a+b+a+c+d+e)
+// function userInformation = 
+
 //   }
 // 
 // console.log(userInformation)
 // console.log(hobbies[index]);
 //// 2. Instruction
-let x = 5; 
-let y = 20;
-let a = x+y;
-
-console.log(a);
-
-let p = (x += y);
-console.log(p);
-
-let z = (x -= y);
-console.log(z);
-
-let e = (x *= y);
-console.log(e);
-
-let f = (x /= y);
-console.log(f);
+// let x = 5; 
+// let y = 20;
+// let a = x+y;
+// 
+// console.log(a);
+// 
+// let p = (x += y);
+// console.log(p);
+// 
+// let z = (x -= y);
+// console.log(z);
+// 
+// let e = (x *= y);
+// console.log(e);
+// 
+// let f = (x /= y);
+// console.log(f);
 // 
 // let z = (x -= y);
 // console.log(z);
 
-        let i =0;
-        while (i <=5) {
-       console.log(i+1);
-       i++;
-       let j=0;
-        while (j <= 5)
-       {
-         console.log(j+1)
-        j++;
-      }
-      }
-
-          let m =10;
-          while (m >= 5) {
-          console.log(m-1);
-          m--;
-          let n=0;
-          while (n >= 10)
-          {
-           console.log(n-1)
-          n--;
-          }
-          }
+        // let i =0;
+        // while (i <=5) {
+    //    console.log(i+1);
+    //    i++;
+    //    let j=0;
+        // while (j <= 5)
+    //    {
+        //  console.log(j+1)
+        // j++;
+    //   }
+    //   }
+// 
+        //   let m =10;
+        //   while (m >= 5) {
+        //   console.log(m-1);
+        //   m--;
+        //   let n=0;
+        //   while (n >= 10)
+        //   {
+        //    console.log(n-1)
+        //   n--;
+        //   }
+        //   }
  ////////////////////////////////////////////////////////////     
  /* 3. Instruction
     let color = prompt ("Guest my favorite color:");
@@ -120,9 +118,9 @@ console.log(f);
  
 /// 5. Instruction
 
-for( i = 20; i >= 0; i--) {
-    console.log(i);
-}
+// for( i = 20; i >= 0; i--) {
+    // console.log(i);
+// }
 
               
 ///6. Instruction 
@@ -146,24 +144,111 @@ for( i = 20; i >= 0; i--) {
  
 // 7 Instruction
 
-const userPass = 'discovery';
-const userName = 'student@mail.com';
-let inputPass = prompt('Enter a username or email:');
+ const userPass = 'discovery';
+ const userName = 'student@mail.com';
+ let inputPass = prompt(`Enter a username or email:`); 
+ 
+ while (inputPass !== userName ) 
+       {
+        inputPass = prompt(`Enter a username or email:(student@mail.com)`);
+       }
 
-while (inputPass != userName ) {
-    inputPass = prompt(`Enter a username or email::`);
-}
-
-    
-   
              let inputPass2 = prompt('Enter a password:');       
-             let counter = 0;
-            while (inputPass2 != userPass && ++counter < 3) {
-                inputPass2 = prompt(`Login Failed Now Only ${counter} Login Attempts Available:`);
-            }
+             let counter = 3;
+             while (inputPass2 !== userPass && counter !== 0) {
+             inputPass2 = prompt(`Login Failed Now Only ${counter} Login Attempts Available:`);
+               --counter;
+             }
                
-            if (counter == 3) {
-                alert('Your account has been blocked!');
-            } else {
-                alert('SuccessFully Login');
-            }  
+             if (counter !== 0) {
+                 alert('SuccessFully Login');
+             } else {
+             alert(`Login Failed Now Only ${counter} Login Attempts Available:`);
+             }  
+        
+
+ /* Hello   World
+ //////////////////////
+ First Name: John
+ Last Name: Smith
+ Age: 30
+ Hobbies : ["Biking"," Mountain Climbing". "Swimming"]
+ Work Address:
+ city: Licoln
+ house Number 32
+ state: Nembraska
+ street Wasington
+ ///////////////////     
+ 
+ John Smith ia 30 years of age
+ His hobies are: eating. sleeping
+*/
+//  let userName = "student@mail.com";
+//  let password = "discovery";
+//  let logInCount = 3;
+// 
+//  while (logInCount != 0) 
+//  {
+// let userName2 = prompt("Enter username or email(student@mail.com):");
+// let password2 = prompt("Enter password(discovery):");
+// 
+    // if (logInCount > 0){
+            //  if ((userName2 !== userName) && (password2 !== password)){
+// 
+                // logInCount--;
+                // alert(`Log-in failed. You now have only ${logInCount} login attempts `);
+                // console.log(`Login failed. You nnow haveonly ${logInCount}login attempts `);
+// 
+                //    }
+            //   else if ((userrname2 === username) && (password2 === password)) {
+                // alert (`Login successful! `);
+                // console.log(`Login successful! `);
+                // break;
+            //   }
+        // }
+        // function myFunction() {
+//   
+            // return
+            //  "
+            // Hello
+            // ";
+            // }
+            // document.getElementById("demo").innerHTML = myFunction();    }
+
+// activity 2
+// console.log(" Hello World!"); 
+// let firstName = "John";
+// let lastName = "Smith";
+// let age = "30";
+// let hobby = {hobbies:["Biking", "Mountain Climbing", "Swimming"]}
+// let relationship = {isMarried:true}
+// const city = "Lincoln";
+// const houseNumber = "32";
+// const state = "Nebraska";
+// const street = " Washington";
+// 
+//let userList = ("");
+// console.log(` First Name: ${firstName}\n Last Name: ${lastName}\n Age: ${age}\n Hobbies: ${hobby.hobbies.length} ${hobby.hobbies} \n Work Address:\n city:${city}\n house Number ${houseNumber}\n state:${state}\n street:${street} `);
+// function userInformation(userList){ console.log(` ${firstName} ${lastName} is ${age} years of age`);
+//        return (userList); }
+// userInformation(userList);       
+// 
+// let otherHobbies = {hobbies2:["eating","sleeping","bingewatching"]}
+//
+// let otherHobby = ("");
+//
+// function myOtherHobby (otherHobby){ console.log (` His hobbies are: ${otherHobbies.hobbies2}`);
+//        return (otherHobby)
+//         };
+//  myOtherHobby(otherHobby);
+// 
+// let myAddress = ("");
+//  function myCompleteAddress(myAddress){ console.log (` city: ${city}\n house number: ${houseNumber}\n state: ${state}\n street: ${street}`);
+//  return (myAddress)
+//   };
+//   myCompleteAddress(myAddress);
+//   console.log(" the value of isMarried is:");
+//   console.log ( relationship.isMarried);
+// 
+// console.log(userInformation)
+// console.log(hobbies[index])
