7581a83f0d832fbde9f54bcb32ae31716a850a0e
Joel Siroy <joel.siroy@gmail.com>
2023-04-13 15:50:23 +0800

    final

--- pages/Technical Assessment/script.js
+++ pages/Technical Assessment/script.js
@@ -1,32 +1,38 @@
-// console.log("Hello World!");
-// 
-// 
-// 
-    // let firstName = "John";
-    // let lastName = "Smith";
-    // let age = "30";
-    // let hobby = {
-        // hobbies:["Biking", "Mountain Climbing", "Swimming"]
-        //    };
-// 
-// console.log(firstName);
-// console.log(lastName);
-// console.log(age);
-// console.log(hobby.hobbies);
-// isMarried: True;
-// 
+// // Instruction 1
+// console.log(" Hello World!");
+// let firstName = "John";
+// let lastName = "Smith";
+// let age = "30";
+// let hobby = {hobbies:["Biking", "Mountain Climbing", "Swimming"]}
+// let relationship = {isMarried:true}
 // const city = "Lincoln";
 // const houseNumber = "32";
 // const state = "Nebraska";
 // const street = " Washington";
-// 
-// function userInformation = 
 
-//   }
-// 
-// console.log(userInformation)
-// console.log(hobbies[index]);
-//// 2. Instruction
+// let userList = ("");
+// console.log(` First Name: ${firstName}\n Last Name: ${lastName}\n Age: ${age}\n Hobbies: ${hobby.hobbies.length} ${hobby.hobbies} \n Work Address:\n city:${city}\n house Number ${houseNumber}\n state:${state}\n street:${street} `);
+// function userInformation(userList){ console.log(` ${firstName} ${lastName} is ${age} years of age`);
+// return (userList); }
+// userInformation(userList);
+
+// let otherHobbies = {hobbies2:["eating","sleeping","bingewatching"]}
+
+// let otherHobby = ("");
+
+// function myOtherHobby (otherHobby){ console.log (` His hobbies are: ${otherHobbies.hobbies2}`);
+// return (otherHobby)
+// };
+// myOtherHobby(otherHobby);
+
+// let myAddress = ("");
+// function myCompleteAddress(myAddress){ console.log (` city: ${city}\n house number: ${houseNumber}\n state: ${state}\n street: ${street}`);
+// return (myAddress)
+// };
+// myCompleteAddress(myAddress);
+// console.log(" the value of isMarried is:");
+// console.log ( relationship.isMarried);
+// //// 2. Instruction
 // let x = 5; 
 // let y = 20;
 // let a = x+y;
@@ -144,28 +150,28 @@
  
 // 7 Instruction
 
- const userPass = 'discovery';
- const userName = 'student@mail.com';
- let inputPass = prompt(`Enter a username or email:`); 
- 
- while (inputPass !== userName ) 
-       {
-        inputPass = prompt(`Enter a username or email:(student@mail.com)`);
-       }
-
-             let inputPass2 = prompt('Enter a password:');       
-             let counter = 3;
-             while (inputPass2 !== userPass && counter !== 0) {
-             inputPass2 = prompt(`Login Failed Now Only ${counter} Login Attempts Available:`);
-               --counter;
-             }
-               
-             if (counter !== 0) {
-                 alert('SuccessFully Login');
-             } else {
-             alert(`Login Failed Now Only ${counter} Login Attempts Available:`);
-             }  
-        
+//  const userPass = 'discovery';
+//  const userName = 'student@mail.com';
+//  let inputPass = prompt(`Enter a username or email:`); 
+//  
+//  while (inputPass !== userName ) 
+    //    {
+        // inputPass = prompt(`Enter a username or email:(student@mail.com)`);
+    //    }
+// 
+            //  let inputPass2 = prompt('Enter a password:');       
+            //  let counter = 3;
+            //  while (inputPass2 !== userPass && counter !== 0) {
+            //  inputPass2 = prompt(`Login Failed Now Only ${counter} Login Attempts Available:`);
+            //    --counter;
+            //  }
+            //    
+            //  if (counter !== 0) {
+                //  alert('SuccessFully Login');
+            //  } else {
+            //  alert(`Login Failed Now Only ${counter} Login Attempts Available:`);
+            //  }  
+        // 
 
  /* Hello   World
  //////////////////////
@@ -214,41 +220,336 @@
             // ";
             // }
             // document.getElementById("demo").innerHTML = myFunction();    }
+  
+//             
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+//             
+// ===========ANSWER ==========
+console.log("=========INSTRUCTION #1===================");
+/*
+Use an external JS file and ensure that it is connected to index.html.
+Assign a variable for first name, last name, age, and hobbies using the proper naming convention.
+Create a constant variable for a city, house number, state, and street.
+Create a function for User Information, Hobbies, and Relationship Status.
 
-// activity 2
-// console.log(" Hello World!"); 
-// let firstName = "John";
-// let lastName = "Smith";
-// let age = "30";
-// let hobby = {hobbies:["Biking", "Mountain Climbing", "Swimming"]}
-// let relationship = {isMarried:true}
+*/
+
+// console.log("Hello World!");
+
+// let fName;
+// let lName;
+// let age;
+// let hobbies;
+
 // const city = "Lincoln";
-// const houseNumber = "32";
+// const hNumber = 32;
 // const state = "Nebraska";
-// const street = " Washington";
-// 
-//let userList = ("");
-// console.log(` First Name: ${firstName}\n Last Name: ${lastName}\n Age: ${age}\n Hobbies: ${hobby.hobbies.length} ${hobby.hobbies} \n Work Address:\n city:${city}\n house Number ${houseNumber}\n state:${state}\n street:${street} `);
-// function userInformation(userList){ console.log(` ${firstName} ${lastName} is ${age} years of age`);
-//        return (userList); }
-// userInformation(userList);       
-// 
-// let otherHobbies = {hobbies2:["eating","sleeping","bingewatching"]}
-//
-// let otherHobby = ("");
-//
-// function myOtherHobby (otherHobby){ console.log (` His hobbies are: ${otherHobbies.hobbies2}`);
-//        return (otherHobby)
-//         };
-//  myOtherHobby(otherHobby);
-// 
-// let myAddress = ("");
-//  function myCompleteAddress(myAddress){ console.log (` city: ${city}\n house number: ${houseNumber}\n state: ${state}\n street: ${street}`);
-//  return (myAddress)
-//   };
-//   myCompleteAddress(myAddress);
-//   console.log(" the value of isMarried is:");
-//   console.log ( relationship.isMarried);
-// 
-// console.log(userInformation)
-// console.log(hobbies[index])
+// const street = "Washington";
+
+// function userInfo (fName, lName, age, hobbies, city, hNumber, state, street)
+// {
+//     hobbies = ["Biking", "Mountain Climbing", "Swimming"];
+
+//     console.log (
+//         `First Name:`, fName,
+//         `\nLast Name:`, lName,
+//         `\nAge:`, age,
+//         `\nHobbies:`, hobbies,
+//         `\nWork Address:`,
+//         `\ncity:`, city,
+//         `\nhouse Number`, hNumber,
+//         `\nstate:`, state,
+//         `\nstreet:`, street
+
+//     );
+// }
+
+// userInfo ( `John`,
+//             `Smith`,
+//             `30`,
+//             hobbies,
+//             city,
+//             hNumber,
+//             state,
+//             street
+// );
+
+// function uInfo (fName, lName, age)
+// {
+//     console.log (fName, lName, `is`, age, `years of age`);
+//     return (fName, lName, age)
+// }
+
+// function hobby (hobbies) {
+//     hobbies = ["eating", "sleeping", "bingewatching"];
+//     return hobbies.toString();
+// }
+
+// console.log(`His hobbies are:`, hobby());
+
+// let city1 = `Lincoln`;
+// let house1 = 32;
+// let state1 = `Nebraska`;
+// let street1 = `Washington`;
+
+// console.log(
+
+//     `city: ${city}`,
+//     `\nhouse number: ${house1}`,
+//     `\nstate: ${state1}`,
+//     `\nstreet ${street1}`
+
+// );
+
+// let isMarried;
+
+// function relStat(isMarried) {
+//     console.log ("The value of isMarried is:");
+//     return (isMarried==isMarried);
+// }
+// console.log(relStat());
+
+console.log ("=====INSTRUCTION #2=====");
+/*
+  Set two variables equal to two different numbers. Use a comparison operator to compare these two variables.
+   Change one of their values by using the +=, -=, *=, or /= operator. Then, compare their values again.
+   Try out the <= and >= operators.
+
+*/
+//answer to number 2
+// let num1 = 10;
+// let num2 = 20;
+
+// console.log (num1 > num2);
+// console.log(num1 < num2);
+// console.log(num1 == num2);
+// console.log(num1 === num2);
+// console.log (num1 >= num2);
+// console.log (num1 <= num2);
+// num1+=30;
+// num1-=40;
+// num1*=50;
+// num1/=60;
+// console.log(num1);
+// console.log(num1 !== num2);
+
+console.log("=====INSTRUCTION #3========")
+/*
+Put the conditional statement inside the function.
+Ensure that the proper NAMING STANDARD is followed.
+Create a Switch Statement that accepts the colors: Black, Violet, and Pink.
+Lowercase and Uppercase letters must be accepted
+
+*/
+//answer to number 3
+
+// let color = prompt ("Enter the color Black, Violet, and Pink");
+// function inputColor (color) {
+//     switch (color){
+//         case (color = "BLACK"):
+//             console.log("the color is " + color);
+//             break;
+//         case (color = "black"):
+//             console.log("the color is " + color);
+//             break;
+//         case (color = "VIOLET"):
+//             console.log("the color is " + color);
+//             break;
+//         case (color = "violet"):
+//             console.log("the color is " + color);
+//             break;
+//         case (color = "PINK"):
+//             console.log("the color is " + color);
+//             break;
+//         case (color = "pink"):
+//             console.log("the color is " + color);
+//             break;
+//         default:
+//             console.log("Error: Invalid color");
+//     }
+// }
+// inputColor(color);
+
+console.log ("====INSTRUCTION #4=====");
+/*
+Create a function that will accept a number, if the number given by the user is an odd number,
+display "odd number". If even number, display "even number".
+Before logging an odd or even number, check the input of the user. 
+If the user input is a string such as "one", "two", "or three", display unexpected input.
+Try these arguments
+5
+10
+"8"
+"20"
+"seven"
+"Fifteen"
+true
+null
+undefined
+NaN
+
+*/
+
+//answer to number 4
+//let num = prompt ("Enter A Number:");
+function oddOrEven (num) {
+    if (isNaN(num) || typeof parseInt(num) !== "") {
+        return `Unexpected Input`
+    } else {
+        if (num % 2 == 0) {
+            return ("Even Number");
+        } else {
+            return ("Odd Number")
+        }
+    }
+}
+
+let userInput = prompt ("Give me a number");
+console.log(oddOrEven(userInput));
+alert(oddOrEven(userInput));
+  console.log("==========INSTRUCTION #5============");
+/*
+Create a loop where it will count from 20 to 0.
+Display the number in the console if it is an even number.
+
+*/
+
+//answer to number 5
+// let b = 20;
+
+// for (b; b>=0; b--) {
+//     if (b % 2 === 0) {
+//         console.log(`number: ${b}`);
+//     }
+// }
+/*
+STRETCH GOALS
+	
+Create a prompt that will let the user enter values from 0 to 20.
+Create a loop where it will count from 20 to 0.
+Display the number in the console if it is an even number.
+Enclosed is your code inside a function.
+*/
+// function counting (b) {
+//     for (b; b>=0; b--) {
+//         if (b % 2 == 0) {
+//             console.log(b);
+//         }
+//     }
+// }
+// let userInput = prompt ("Enter a number from 0 to 20");
+// counting(userInput);
+//  console.log("========INSTRUCTION #6============")
+ /*
+ Prompt a user to enter a number.
+ When a number is less than 50, we need to display the message that we need to terminate the loop.
+ When a number is divisible by 10, display a message that it is divisible by 10. Therefore, they will skip the number.
+ If it is divisible by 5, display the number.
+ */
+// //answer to number 6
+// let number = prompt ("Give me a number");
+
+// for (number; number >= 0; number--)
+// {
+//     if(number <= 50)
+//     {
+//         console.log(`Currently at number ${number}. Terminating the loop `)
+//     }
+
+//     if (number % 10 === 0){
+//         console.log("The number is divisible by 10. Skipping the number");
+//     }
+
+//     if (number % 5 === 0) {
+//         console.log(number)
+//     }
+// }
+console.log("==========INSTRUCTION #7==========")
+/*
+Login attempts using JS Loops
+The goal for this task is to ensure that nested if statements are implemented.
+This will be your baseline for your account security.
+The mock login lets the user attempt logging in three times.
+
+Username: student@mail.com
+Password: discovery
+
+Create a prompt where a user will enter a username/email.
+Create a prompt where a user will enter a password.
+
+When a user enters the correct username and password, 
+it will alert us that the login is successful.
+
+When a user enters an incorrect username and password,
+ it will display several attempts until it reaches zero.
+*/
+//answer to number 7
+// nested IFs
+// let login_attempts = 3;
+// let uname = prompt ("Enter a username or email (student@mail.com)");
+// let pass = prompt ("Enter a password (discovery)");
+
+// function login(uname, pass) {
+//     if(uname == "student@mail.com" && pass == "discovery")
+//     {
+//         alert ("Sucessfully login");
+//     }
+//     else {
+//         login_attempts = login_attempts -1;
+//         alert ("Login Failed Now Only" + login_attempts + "Login Attempts Available");
+//         let uname = prompt ("Enter a username or email (student@mail.com)");
+//         let pass = prompt ("Enter a password (discovery)");
+
+//         if(uname == "student@mail.com" && pass == "discovery")
+//         {
+//             alert ("Sucessfully login");
+//         }
+//         else {
+//             login_attempts = login_attempts -1;
+//             alert ("Login Failed Now Only" + login_attempts + "Login Attempts Available");
+//             let uname = prompt ("Enter a username or email (student@mail.com)");
+//             let pass = prompt ("Enter a password (discovery)");
+
+//             if(uname == "student@mail.com" && pass == "discovery")
+//             {
+//                 alert ("Sucessfully login");
+//             }
+
+//             else {
+//                 login_attempts = login_attempts -1;
+//                 alert ("Login Failed Now Only" + login_attempts + "Login Attempts Available");
+//             }
+            
+
+//         }
+//     }
+// }
+
+// login(uname, pass);
+
+//loops
+// let i= 3;
+// while (i !=0) //loop runs 3 times
+// {
+//     let userName = "student@mail.com";
+//     let uName = prompt ("Enter username or email");
+//     let userPassword = "discovery";
+//     let pass = prompt ("Enter a password");
+
+//     if (userName == uName && userPassword == pass)
+//     {
+//         i = 4-i;
+//         alert("Sucessfully login");
+//         break;
+//     }
+//     else {
+//         i = i-1;
+//         alert ("Login Failed Now Only" + i + "Login Attempts Available")
+//     }
+// }
